1f4e5b5e3ecd5d900e7fdaf7a9d1715a87097268
Ricardo031 <rjtb31@gmail.com>
2024-10-17 12:46:08 -0500

    correcciones
    solo ajuste los tamaños y algunas palabras

--- js/main.js
+++ js/main.js
@@ -9,15 +9,16 @@ btn.addEventListener('click', (e) => {
         return;
     }
     
-    let copia = text.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let copia = text.toLowerCase().replace(/[^a-z0-9]/g, '');//*eliminar todo los caracteres que no sean de a-z o numeros de 0-9
+    //g:global, búsqueda se hace en toda la cadena y no se detiene en la primera coincidencia.
 
-    // Invertir el texto procesado
-    let reversedText = copia.split('').reverse().join('');
+    //* Invertir el texto procesado
+    let reversedText = copia.split('').reverse().join(''); //copia.split es para separar las letras en una cadena string y se divide por las '', el reverse es para voltearlos y ya luego con el join vuelve a unir los caracteres del array en una cadena de texto. El argumento '' indica que los caracteres se deben juntar sin ningún separador entre ellos.
 
-    // Verificar si es un palíndromo
+    //* Verificar si es un palíndromo
     if (copia === reversedText) {
-        result.textContent = `"${text}" es un palindrome.`;
+        result.textContent = `"${text}" Es un palindromo.`;
     } else {
-        result.textContent = `"${text}" no es un palindrome.`;
+        result.textContent = `"${text}" NO es un palindromo.`;
     }
 });
